d0328d708f3faa2ff5cab43fa24ce10d489404fa
Youngjun Na <ruminoreticulum@gmail.com>
2017-10-17 10:08:02 +0900

    Create Journal_of_animal_science.R

---
+++ Journal_of_animal_science.R
@@ -0,0 +1,27 @@
+# Scraping the data from Scientific Journals
+
+library(rvest)
+library(dplyr)
+library(stringr)
+
+#Journal of Animal sciecne
+url <- "https://www.animalsciencepublications.org/publications/jas/articles/95/1/9"
+html <- read_html(url, encoding="UTF-8")
+
+#keywords
+keywords<-html %>% html_nodes(".kwd-group") %>% html_children() %>% html_text()  
+keywords<-gsub(";","",keywords) %>% data.frame()
+
+keywords
+
+#history
+history<-html %>% html_nodes(".history-list") %>% html_children() %>% html_text() 
+
+recieved<-gsub(",","",str_sub(history[1],start=11))
+accepted<-gsub(",","",str_sub(history[2],start=11))
+published<-gsub(",","",str_sub(history[3],start=12))
+
+data.frame(recieved=recieved,accepted=accepted,published=published)
+
+#contributor list
+contributor<-html %>% html_nodes(".contributor-list") %>% html_children() %>% html_text()
