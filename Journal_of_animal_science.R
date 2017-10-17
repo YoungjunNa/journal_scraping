3aa479e928e305ecff94f4bdd7fd7c368a84efd9
Youngjun Na <ruminoreticulum@gmail.com>
2017-10-17 10:58:05 +0900

    Update Journal_of_animal_science.R

--- Journal_of_animal_science.R
+++ Journal_of_animal_science.R
@@ -1,26 +1,39 @@
-# Scraping the data from Scientific Journals
+## Scraping the data from Scientific Journals
+
 library(rvest)
 library(dplyr)
 library(stringr)
+library(lubridate)
 
 #Journal of Animal sciecne
-url <- "https://www.animalsciencepublications.org/publications/jas/articles/95/1/9"
+vol<-95
+issue<-2
+page<-789
+
+url <- paste0("https://www.animalsciencepublications.org/publications/jas/articles/",vol,"/",issue,"/",page)
 html <- read_html(url, encoding="UTF-8")
 
 #keywords
 keywords<-html %>% html_nodes(".kwd-group") %>% html_children() %>% html_text()  
-keywords<-gsub(";","",keywords) %>% data.frame()
+keywords<-gsub(";","",keywords)
+keywords<-data.frame(vol=vol, issue=issue, page=page,keywords=keywords)
 
 keywords
 
 #history
 history<-html %>% html_nodes(".history-list") %>% html_children() %>% html_text() 
 
-recieved<-gsub(",","",str_sub(history[1],start=11))
-accepted<-gsub(",","",str_sub(history[2],start=11))
-published<-gsub(",","",str_sub(history[3],start=12))
+recieved<-gsub(",","",str_sub(history[1],start=11)) %>% as.character()
+accepted<-gsub(",","",str_sub(history[2],start=11)) %>% as.character()
+published<-gsub(",","",str_sub(history[3],start=12)) %>% as.character()
 
-data.frame(recieved=recieved,accepted=accepted,published=published)
+history<-data.frame(recieved=recieved,accepted=accepted,published=published, corresponding=NA)
 
-#contributor list
-contributor<-html %>% html_nodes(".contributor-list") %>% html_children() %>% html_text()
+history
+
+#corresponding_author
+corresponding<-html %>% html_node(".fn-corresp") %>% html_children() %>% .[[2]] %>% html_text() 
+
+history[,4]<-corresponding
+
+history
