233333f4920379b229a70c061f40169f28fee4a5
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-06 11:31:30 +0900

    some bugs is fixed

--- Journal_of_animal_science.R
+++ Journal_of_animal_science.R
@@ -9,10 +9,14 @@ library(dplyr)
 library(stringr)
 library(lubridate)
 
+#data.frame
+keywords<-data.frame(vol=vol, issue=issue, page=page,keywords=keywords)
+history<-data.frame(recieved=recieved,accepted=accepted,published=published, corresponding=NA)
+
 #Journal of Animal sciecne
 vol<-95
-issue<-2
-page<-789
+issue<-10
+page<-4239
 
 url <- paste0("https://www.animalsciencepublications.org/publications/jas/articles/",vol,"/",issue,"/",page)
 html <- read_html(url, encoding="UTF-8")
@@ -20,18 +24,23 @@ html <- read_html(url, encoding="UTF-8")
 #keywords
 keywords<-html %>% html_nodes(".kwd-group") %>% html_children() %>% html_text()  
 keywords<-gsub(";","",keywords)
-keywords<-data.frame(vol=vol, issue=issue, page=page,keywords=keywords)
-
 keywords
 
 #history
 history<-html %>% html_nodes(".history-list") %>% html_children() %>% html_text() 
 
-recieved<-gsub(",","",str_sub(history[1],start=11)) %>% as.character()
-accepted<-gsub(",","",str_sub(history[2],start=11)) %>% as.character()
-published<-gsub(",","",str_sub(history[3],start=12)) %>% as.character()
+recieved<-gsub(",","",str_sub(history[1],start=11)) 
+accepted<-gsub(",","",str_sub(history[2],start=11)) 
+published<-gsub(",","",str_sub(history[3],start=12)) 
 
-history<-data.frame(recieved=recieved,accepted=accepted,published=published, corresponding=NA)
+recieved <- gsub("\u00A0", " ", recieved) #solve the problem of <u+00a0>. thank you enersto Huang (https://stackoverflow.com/users/7549197/enersto-huang)
+accepted <- gsub("\u00A0", " ", accepted)
+
+#numMonth <- function(x) {
+#  months <- list(Jan=1,Feb=2,Mar=3,Apr=4,May=5,Jun=6,July=7,Aug=8,Sep=9,Oct=10,Nov=11,Dec=12)
+#  x <- tolower(x)
+#  sapply(x,function(x) months[[x]])
+#}
 
 history
 
@@ -41,3 +50,7 @@ corresponding<-html %>% html_node(".fn-corresp") %>% html_children() %>% .[[2]] 
 history[,4]<-corresponding
 
 history
+
+#write.csv
+
+write.csv(history,"history.txt",row.names = FALSE)
