0cd6e189f2250a9abc4d3e9ad08c7c3f70ddbf20
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-15 10:42:14 +0900

    version 1.0

--- Journal_of_animal_science.R
+++ Journal_of_animal_science.R
@@ -2,49 +2,50 @@
 #Author: Youngjun Na
 #Email: ruminoreticulum@gmail.com
 #Github: https://github.com/YoungjunNa
-#Date: 11/2/2017
+#Last update: 11/15/2017
 
 library(rvest)
 library(dplyr)
 library(stringr)
 library(lubridate)
 
+#URL list
+setwd("D:/GitHub/journal_scraping") #set working directory
+list <- readxl::read_excel("journal_URL.xlsx") #set URL list
+list <- filter(list, journal == "journal of animal science") #filtering the journal
+
 #data.frame
-keywords<-data.frame(vol=vol, issue=issue, page=page,keywords=keywords)
-history<-data.frame(recieved=recieved,accepted=accepted,published=published, corresponding=NA)
+JAS_result<-list
+JAS_result<-cbind(JAS_result, subject=NA)
+JAS_result<-cbind(JAS_result, keywords=NA)
+JAS_result<-cbind(JAS_result, corresponding=NA)
 
 #Journal of Animal sciecne
-vol<-95
-issue<-10
-page<-4239
-
-url <- paste0("https://www.animalsciencepublications.org/publications/jas/articles/",vol,"/",issue,"/",page)
+url <- list$URL[1]
 html <- read_html(url, encoding="UTF-8")
 
+#Subject
+subject<-html %>% html_nodes(xpath="//div/h1/text()") %>% html_text()  
+JAS_result$subject[1] <- subject
+
 #keywords
-keywords<-html %>% html_nodes(".kwd-group") %>% html_children() %>% html_text()  
+keywords<-html %>% html_nodes(".kwd-group") %>% html_text()  
 keywords<-gsub(";","",keywords)
-keywords
-
-#history
-history<-html %>% html_nodes(".history-list") %>% html_children() %>% html_text() 
-
-recieved<-gsub(",","",str_sub(history[1],start=11)) 
-accepted<-gsub(",","",str_sub(history[2],start=11)) 
-published<-gsub(",","",str_sub(history[3],start=12)) 
-
-recieved <- gsub("\u00A0", " ", recieved) #solve the problem of <u+00a0>. thank you enersto Huang (https://stackoverflow.com/users/7549197/enersto-huang)
-accepted <- gsub("\u00A0", " ", accepted)
-
-history
+keywords<-gsub("\n\t","",keywords)
+JAS_result$keywords[1] <- keywords
 
 #corresponding_author
 corresponding<-html %>% html_node(".fn-corresp") %>% html_children() %>% .[[2]] %>% html_text() 
-
-history[,4]<-corresponding
-
-history
+JAS_result$corresponding[1]<-corresponding
 
 #write.csv
-
 write.csv(history,"history.txt",row.names = FALSE)
+
+##history
+#history<-data.frame(recieved=recieved,accepted=accepted,published=published, corresponding=NA)
+#history<-html %>% html_nodes(".history-list") %>% html_children() %>% html_text() 
+#recieved<-gsub(",","",str_sub(history[1],start=11)) 
+#accepted<-gsub(",","",str_sub(history[2],start=11)) 
+#published<-gsub(",","",str_sub(history[3],start=12)) 
+#recieved <- gsub("\u00A0", " ", recieved) #solve the problem of <u+00a0>. thank you enersto Huang (https://stackoverflow.com/users/7549197/enersto-huang)
+#accepted <- gsub("\u00A0", " ", accepted)
