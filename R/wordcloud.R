c27c3966e9d1f0970802d322aa066816c6d21058
YoungjunNa <ruminoreticulum@gmail.com>
2017-11-17 15:23:52 +0900

    fix wordcloud.R

--- R/wordcloud.R
+++ R/wordcloud.R
@@ -24,7 +24,7 @@ wc_data<-tm_map(wc_data, tolower)
 wc_data<-tm_map(wc_data,removeNumbers)
 wc_data<-tm_map(wc_data, removePunctuation)
 wc_data<-tm_map(wc_data, removeWords, stopwords("english"))
-wc_data<-tm_map(wc_data, removeWords, c("affect","effect","effects","and","the","our","that","for","are","also","more","has","must","have","should","this","with","performance","dietary","supplementation"))
+wc_data<-tm_map(wc_data, removeWords, c("affect","effect","effects","and","the","our","that","for","are","also","more","has","must","have","should","this","with"))
 
 tdm_wc<-TermDocumentMatrix(wc_data) #Creates a TDM
 TDM1<-as.matrix(tdm_wc) #Convert this into a matrix format
@@ -41,4 +41,6 @@ d <- data.frame(word = names(v),freq=v)
 head(d, 10)
 
 barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word, col ="lightblue", main ="Most frequent words", ylab = "Word frequencies")
+
+
 
