29f94b44dc32f9ebb12b5915c44fe82c568cc887
YoungjunNa <ruminoreticulum@gmail.com>
2017-12-15 16:32:13 +0900

    add google translate

--- R/googleLanguageR.R
+++ R/googleLanguageR.R
@@ -4,7 +4,7 @@ GL_AUTH="C:/Users/Youngjun/Google Drive/Google_cloud/translateR-079f74265489.jso
 library(googleLanguageR)
 gl_auth("C:/Users/Youngjun/Google Drive/Google_cloud/translateR-079f74265489.json")
 
-result <- read.csv("journal_result_2017_12.txt")
+result <- read.csv("journal_result.txt")
 result$subject <- as.character(result$subject)
 result$subject <- as.vector(result$subject)
 
@@ -23,13 +23,24 @@ for(i in 0:(n-1)){
   result$google_translate[i] <- translated[1]
 }
 
-
 for(i in 0:(n-1)){
+  i+1
+  a <- result$subject[i]
+  translated <- gl_translate(a, target = "ko")
+  result$google_translate[i] <- translated[1]
+  Sys.sleep(3.0)
+}
+
+ i <- 0
+repeat{
   i+1
   translated <- gl_translate(result$subject[i], target = "ko")
   result$google_translate[i] <- translated[1]
+    if(i==(n-1)) break
 }
 
 
-gl_translate(result$subject[11], target = "ko")[1]
+gl_translate(result$subject[40], target = "ko")[1]
+gl_nlp(result$subject[30])
+
 
